e510867d9ae4fce2995e100ce10fa71257c40ab9
Julia E. Rager's Research Group <72747901+Ragerlab@users.noreply.github.com>
2022-06-15 13:50:52 -0400

    Add files via upload

---
+++ Experiment_3/scripts/4h_transcriptomic_similarity_FC_2.R
@@ -0,0 +1,188 @@
+rm(list=ls())
+
+library(vegan)
+library(janitor)
+library(xlsx)
+library(readxl)
+library(reshape2)
+library(tidyverse)
+library(cluster)
+library(pheatmap)
+library(factoextra)
+
+
+setwd("/Users/lkoval/IEHS Dropbox/Rager Lab/Lauren_Koval/LK_Lab_Notebook/Projects/Transcriptomic_Similarity/Sensitivity_Analysis/Experiment_3/input/4h")
+
+
+#Create dataframe to store all relevent data from all input files (gene, adjusted pvalue, log2 fold change, and the exposure condition)
+df4 <- data.frame(gene=character(),
+                  padj=numeric(),
+                  log2FoldChange=numeric(),
+                  expo_cond=character())
+
+
+#Get list of input files from working directory
+files_4 <- list.files()
+
+#Read in the statistical results of all exposure conditions and store in previously established dataframe
+for(f in files_4){
+  cond <- str_split(f,"_")[[1]][5]
+  temp <- read_csv(f)
+  temp <- temp %>% dplyr::rename(gene=...1) %>% select(gene,padj,log2FoldChange) %>% mutate(expo_cond=cond)
+  df4 <- rbind(df4,temp)
+}  
+
+
+setwd("/Users/lkoval/IEHS Dropbox/Rager Lab/Lauren_Koval/LK_Lab_Notebook/Projects/Transcriptomic_Similarity/Sensitivity_Analysis/Experiment_3")
+
+
+
+#Filter for all rows that show a gene is significantly differentially expressed, here defined as a pval <0.1 and FC >=2.5, then pull a list of all unique genes that pass this filter
+full_df <- as.data.frame(df4)
+
+
+sig <- full_df %>% filter(expo_cond!="LPS") %>%  filter(padj<0.1 & abs(log2FoldChange)>=log2(2))
+sig <- unique(sig$gene)
+
+
+#Filter the original dataset for the list of unique genes, then create a column for a binary expression indicated by a cutpoint of padj<0.1 and a fold change of 2.5
+#Format the data for clustering by exposure condition such that the rows are the exposure conditions,columns are the genes, and the values are
+#the binary expression indicators.
+df_sig <- full_df %>%
+  filter(gene %in% all_of(sig)) %>% 
+  mutate(padj=if_else(is.na(padj), 100,padj)) %>% 
+  mutate(expr=if_else(padj<0.1 & abs(log2FoldChange)>=log2(2) ,1,0)) %>%
+  select(!c("padj","log2FoldChange")) %>% 
+  pivot_wider(names_from = gene, values_from = expr) %>%
+  column_to_rownames("expo_cond")
+
+# Create a jaccard distance matrix for exposure conditions
+samp_comp <- mutate_all(df_sig, function(x) as.numeric(as.character(x)))
+D_samp <- vegdist(as.matrix(samp_comp),method = "jaccard")
+
+# make a table of jaccard similarities
+sim_mat <- as.data.frame(as.matrix(D_samp))
+sim_mat <- sim_mat %>% mutate_all(function(x) 1-x)
+
+#read in file with FC >= 2.5 exposure condition groupings and specify where splits should go in heatmap
+ord <- read_csv("input/4h_similarity_lower_tri.csv")
+seprows <- c(2,6,7,9)
+
+
+#Pull the filtered binary data and prepare it for clustering on genes
+gene_comp <- samp_comp
+gene_comp <- as.data.frame(t(gene_comp))
+
+#Create a jaccard distance matrix for genes
+D_gene <- vegdist(as.matrix(gene_comp),method = "jaccard")
+
+#Determine the optimal number of clusters of genes
+# Sys.time()
+# wss_gene <- fviz_nbclust(gene_comp, diss = D_gene, method = "wss", FUN=hcut, hc_func="diana", k.max=100)
+# wssplot_gene <- wss_gene$data
+# wssplot_gene <- wssplot_gene %>% ggplot(aes(x=clusters, y=y))+geom_point()+geom_line(group=1)+labs(x="number of clusters k", y="total within sum of squares")
+# ggsave("figures/4h_optimal_clusters_gene_wss_FC_2.png", height=10, width=16, unit="in",plot=wssplot_gene)
+# Sys.time()
+# silho_gene <- fviz_nbclust(gene_comp, diss = D_gene, method = "silhouette", FUN=hcut, hc_func="diana", k.max=100)
+# silhoplot_gene <- silho_gene$data
+# silhoplot_gene <- silhoplot_gene %>% ggplot(aes(x=clusters, y=y))+geom_point()+geom_line(group=1)+labs(x="number of clusters k", y="average silhouette width")
+# ggsave("figures/4h_optimal_clusters_gene_silhouette_FC_2.png", height=10, width=16, unit="in",plot=silhoplot_gene)
+# Sys.time()
+
+
+#Run hierarchical clustering for genes
+cluster_gene <-diana(D_gene, diss=TRUE)
+plot(cluster_gene)
+
+#Select 69 clusters and get cluster assignment for each gene
+ncut_gene <- 69
+cluster_assignments_gene <- cutree(cluster_gene, k = ncut_gene)
+
+#Number of genes in each cluster
+k_gene <-table(cluster_assignments_gene)
+
+#Add the cluster assignments to genes and arrange the dataframe by the cluster
+gene_comp$cluster <- cluster_assignments_gene
+gene_comp <- gene_comp %>% arrange(cluster)
+
+
+#specify new column order in exposure condition dataframe to reflect clustered genes
+new_col_order <- rownames(gene_comp)
+samp_comp <- samp_comp[new_col_order]
+
+#Organize data such that the clusters are the same as the FC>=2.5 groupings
+samp_comp <- samp_comp %>% rownames_to_column("cond") %>% arrange(factor(cond, levels=ord$ind))
+
+hm_data <- as.data.frame(samp_comp)
+hm_data <- hm_data %>% adorn_totals(where = "col", ... =!c("cond")) %>%  mutate(new_rownames=paste0(cond,"_",Total,"DEGs")) %>% column_to_rownames("cond") 
+new_rownames <- hm_data$new_rownames
+hm_data <- hm_data %>% select(!c("new_rownames","Total"))
+
+
+
+#Make heatmap
+pheatmap(hm_data, main="4h (padj<0.1 & |FC|>=2)",
+         cluster_rows=FALSE,
+         cluster_cols = FALSE,
+         show_rownames = TRUE,
+         show_colnames = FALSE,
+         color = c("gray75","purple2"),
+         breaks = c(0, 0.5, 1),
+         legend = FALSE,
+         cellheight = 50,
+         cellwidth = 1.4,
+         fontsize = 10,
+         angle_col = 45,
+         gaps_row = seprows,
+         labels_row = new_rownames,
+         filename = "figures/4h_omics_heatmap_FC_2.png",
+         height = 24,
+         width = 36,
+         border_color = NA)
+
+
+# format table of jaccard similarities between exposure conditions and include column of the number of overlapping genes that are significant for each
+sim_mat <- sim_mat %>% rownames_to_column("cond") %>% arrange(factor(cond,levels = rownames(hm_data))) %>% column_to_rownames("cond") %>% select(rownames(hm_data))
+
+sim_mat[upper.tri(sim_mat,diag=FALSE)] <- 17
+
+sim_mat_long <- sim_mat %>% rownames_to_column("ind") %>% pivot_longer(!ind, names_to = "comp",values_to = "jaccard_similarity") %>% mutate(merge_col=paste0(ind,"_",comp))
+
+degs <- as.data.frame(t(hm_data))
+conds <- colnames(degs)
+
+res <- data.frame(ind=character(),
+                  comp=character(),
+                  inxn=character())
+
+for(i in 1:length(conds)){
+  ind <- conds[i]
+  for(j in 1:length(conds)){
+    comp <- conds[j]
+    temp <- degs %>% select(ind,comp)
+    overlap <- temp %>% mutate(s=rowSums(.)) %>% filter(s==2)
+    if(ncol(temp)==1){
+      val <- sum(temp[1])
+    }
+    else{
+      val <- nrow(overlap)
+    }
+    r <- c(ind, comp, val)
+    res <- rbind(res, r)
+  }
+}
+
+colnames(res) <- c("ind","comp","overlap")
+
+res <- res %>% mutate(merge_col=paste0(ind,"_",comp)) %>% select(merge_col,overlap)
+
+final_tab <- merge(sim_mat_long,res, by="merge_col", all.x = TRUE)
+final_tab <- final_tab %>% select(!merge_col) %>%
+  mutate(overlap=ifelse(jaccard_similarity==17," ",overlap)) %>% 
+  mutate(jaccard_similarity=ifelse(jaccard_similarity==17," ",jaccard_similarity)) %>% 
+  pivot_wider(names_from = "comp", values_from = c("jaccard_similarity","overlap")) %>% 
+  select(c(1,4,15,5,16,2,13,3,14,8,19,,11,22,7,18,9,20,10,21,6,17,12,23)) %>% 
+  dplyr::slice(match(rownames(sim_mat),ind))
+
+
+write_csv(final_tab, "output/4h_similarity_lower_tri_FC_2.csv")
---
+++ Experiment_3/scripts/sig_genes_4h.R
@@ -0,0 +1,80 @@
+rm(list=ls())
+
+library(janitor)
+library(xlsx)
+library(readxl)
+library(reshape2)
+library(tidyverse)
+
+
+setwd("/Users/lkoval/IEHS Dropbox/Rager Lab/Lauren_Koval/LK_Lab_Notebook/Projects/Transcriptomic_Similarity/Sensitivity_Analysis/Experiment_3/input/4h")
+
+
+#Create dataframe to store all relevent data from all input files (gene, baseMean, adjusted pvalue, log2 fold change, and the exposure condition)
+df4 <- data.frame(gene=character(),
+                  baseMean=numeric(),
+                  padj=numeric(),
+                  log2FoldChange=numeric(),
+                  expo_cond=character())
+
+
+#Get list of input files from working directory
+files_4 <- list.files()
+
+#Read in the statistical results of all exposure conditions and store in previously established dataframe
+for(f in files_4){
+  cond <- str_split(f,"_")[[1]][5]
+  temp <- read_csv(f)
+  temp <- temp %>% dplyr::rename(gene=...1) %>% select(gene,baseMean,padj,log2FoldChange) %>% mutate(cond=cond)
+  df4 <- rbind(df4,temp)
+}  
+
+#Change to main directory
+setwd("/Users/lkoval/IEHS Dropbox/Rager Lab/Lauren_Koval/LK_Lab_Notebook/Projects/Transcriptomic_Similarity/Sensitivity_Analysis/Experiment_3")
+
+
+
+get_sig_genes <- function(df,filt){
+  #Filter for genes that differentially expressed  for any exposure condition
+  full_df <- as.data.frame(df)
+  full_df <- full_df %>% filter(padj<0.1 & abs(log2FoldChange)>=filt)
+  
+  
+  #make mapping of terminology we want to use
+  conds <- unique(full_df$cond)
+  
+  mapping <- data.frame(cond=conds,
+                        expo_cond=c("Flaming Eucalyptus",
+                                    "Smoldering Eucalyptus",
+                                    "LPS",
+                                    "Flaming Peat",
+                                    "Smoldering Peat",
+                                    "Flaming Pine",
+                                    "Flaming Pine Needles",
+                                    "Smoldering Pine Needles",
+                                    "Smoldering Pine",
+                                    "Flaming Red Oak",
+                                    "Smoldering Red Oak"))
+  
+  mapping <- mapping %>% mutate(group_comp=paste0(expo_cond," vs. Saline"))
+  
+  #map in correct terminology to df of DEGs
+  final <- merge(full_df, mapping, by="cond", all.x = TRUE)
+  final <- final %>% mutate(timepoint="4h") %>% select(expo_cond,group_comp,timepoint,gene,baseMean,padj,log2FoldChange)
+  return(final)
+}
+
+
+
+
+fc2 <-get_sig_genes(df4, log2(2))
+fc1.5 <-get_sig_genes(df4, log2(1.5))
+
+
+#write out results
+write_csv(fc2, "output/DEGs_4h_FC_2.csv")
+write_csv(fc1.5, "output/DEGs_4h_FC_1_5.csv")
+
+
+
+
